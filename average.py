a6d7cb8ace1ea2c88cc7592ced905ea4703f5825
Sakshi123u <147252336+Sakshi123u@users.noreply.github.com>
2024-01-08 23:10:51 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,8 @@
+#average of given numbers
+a=int(input("enter english marks :"));
+b=int(input("enter maths marks :"));
+c=int(input("enter physics marks :"));
+d=int(input("enter chem marks"));
+e=int(input("enter IT marks :"));
+avg=(a+b+c+d+e)/5;
+print("average is :",avg);
